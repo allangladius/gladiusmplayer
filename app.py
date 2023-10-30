4b06fb635079d4d3edab3c1d8c6acb32c13821fe
Allan Souza <jeffersonafsouza@gmail.com>
2023-10-29 22:52:39 -0300

    organizacao

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from front_gmp import *
+from back_front_gmp import RenderGui
+from tkinter import *
 
 
 
 
 #Instanciando o Tk
 root = Tk()
-#Argumento passado a classe MusicPlayer para ser a janela principal
+#Argumento passado a classe RenderGui para ser a janela principal
 RenderGui(root)
 #Mantém a janela aberta ouvindo eventos e só fecha ao fechar a janela
 root.mainloop()
